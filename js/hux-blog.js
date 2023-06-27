8b181048bda3884a629f8fd60bf8e3a85e08fbb9
小裙子 <844546673@qq.com>
2023-06-27 15:25:33 +0800

    reset

--- js/hux-blog.js
+++ js/hux-blog.js
@@ -9,6 +9,8 @@ console.log('test3')
 console.log('test4')
 console.log('test5')
 console.log('test6')
+console.log('test7')
+console.log('test8')
 // Tooltip Init
 $(function() {
     $("[data-toggle='tooltip']").tooltip();
